38a75ed83aae6278916ece94a012a959a25e91f9
Om Ashishkumar Soni <oms279400@gmail.com>
2022-08-17 15:17:11 +0530

    shree ganeshay namah , ssv no jay jay kar ho

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,8 @@
+// import { MatIconModule } from '@angular/material/icon';
+// import { MatButtonModule } from '@angular/material/button';
+// import { MatListModule } from '@angular/material/list';
+// import { MatSidenavModule } from '@angular/material/sidenav';
+// import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
@@ -20,6 +25,7 @@ import { SetProblemComponent } from './set-problem/set-problem.component';
 import { TagProblemsComponent } from './tag-problems/tag-problems.component';
 import { ProblemListComponent } from './problem-list/problem-list.component';
 import { DifficultyProblemsComponent } from './difficulty-problems/difficulty-problems.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
@@ -44,6 +50,8 @@ import { DifficultyProblemsComponent } from './difficulty-problems/difficulty-pr
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    
   ],
   providers: [
     CookieService
